4ec30154cace72157d1ae533cc0be51d319186dd
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:55:58 +0400

    Create ShuffleButton component

---
+++ src/components/shared/buttons/ShuffleButton.tsx
@@ -0,0 +1,20 @@
+'use client';
+import { useShuffle } from '@/lib/contexts/ShuffleContext';
+import { faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { clsx } from 'clsx';
+
+export default function ShuffleButton() {
+    const { isShuffling, setIsShuffling } = useShuffle();
+    return (
+        <button
+            className={clsx(
+                'control-btn text-3xl',
+                isShuffling ? 'text-[#1FD760]' : 'text-theme'
+            )}
+            onClick={() => setIsShuffling(!isShuffling)}
+        >
+            <FontAwesomeIcon icon={faShuffle} />
+        </button>
+    )
+}
