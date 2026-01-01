3f115bd3247fb651db9e2d86bdf32e96085911e9
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:44:48 +0400

    Create parallel route for new releases

---
+++ src/app/(home)/@newreleases/page.tsx
@@ -0,0 +1,26 @@
+import { use } from 'react';
+import Grid from '@/components/shared/wrappers/Grid';
+import { getNewReleases } from '@/lib/api/albums/getNewReleases';
+import AlbumCard from '@/components/shared/cards/AlbumCard';
+
+export default function NewReleasesPage() {
+   const {
+      albums: { items },
+   } = use(getNewReleases());
+
+   return (
+      <div>
+         <h1 className="text-3xl mb-6 mt-12 font-bold max-md:text-2xl">New Releases</h1>
+         <Grid>
+            {items.map((album) => {
+               const artists = album.artists.map((artist) => artist.name);
+               return (
+                  <AlbumCard key={album.id} album={album}>
+                     <p className="text-gray-500 line-clamp-1 text-sm mt-0.5">{artists.join(', ')}</p>
+                  </AlbumCard>
+               );
+            })}
+         </Grid>
+      </div>
+   );
+}
