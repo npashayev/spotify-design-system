67a376d9ec164f871f1a0dfe2586ba436335211f
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:38:14 +0400

    Create global loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import Loading from '@/components/Loading';
+
+export default function LoadingPage() {
+   return (
+      <div className="flex flex-col items-center justify-center min-h-page px-4">
+         <Loading />
+      </div>
+   );
+}
