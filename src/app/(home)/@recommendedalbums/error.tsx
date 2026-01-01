38e5c72f3ae056ebfe09986de18e11149aa34535
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:44:20 +0400

    Create error.tsx for recommended albums page

---
+++ src/app/(home)/@recommendedalbums/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import ErrorBox from '@/components/shared/wrappers/ErrorBox';
+
+export default function Error() {
+   return <ErrorBox>Failed to fetch recommended albums. Please try again later.</ErrorBox>;
+}
