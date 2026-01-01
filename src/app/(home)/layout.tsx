35b9acce73ac0572b05c145accc9b731d8b90498
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:48:15 +0400

    Create new layout file and render newreleases and recommendedalbums pages inside it

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,35 @@
+import Loading from '@/components/Loading';
+import { ReactNode, Suspense } from 'react';
+
+interface Props {
+   newreleases: ReactNode;
+   recommendedalbums: ReactNode;
+}
+
+export default function HomePageLayout({newreleases,recommendedalbums}: Props) {
+   return (
+      <main className="p-8 min-h-page">
+         <h1 className="mx-auto mt-4 max-w-3xl text-center text-4xl font-bold max-2xl:text-3xl max-md:text-2xl">
+            Welcome to Spotify, find and listen to your favorite songs here
+         </h1>
+
+         <Suspense fallback={
+               <Loading>
+                  <h2>Recommended albums are loading...</h2>
+               </Loading>
+            }
+         >
+            {recommendedalbums}
+         </Suspense>
+
+         <Suspense fallback={
+               <Loading>
+                  <h2>New releases are loading...</h2>
+               </Loading>
+            }
+         >
+            {newreleases}
+         </Suspense>
+      </main>
+   );
+}
