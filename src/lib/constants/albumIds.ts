e49f094d56bfeb444db788bd52ea300156397c26
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:10:55 +0400

    Create a list of constant album ids to display in the home page as recommended albums list

---
+++ src/lib/constants/albumIds.ts
@@ -0,0 +1,12 @@
+export const albumIds = [
+  "0Y6aeC29PCS6MNymIDM74g",
+  "6lmjyMZTJmkJuQrXo79HM1",
+  "49DV9eFp2xTdtO7veew5xS",
+  "6opKASPALOkPwCfHvqIS9H",
+  "6ZvDJs17O3woQirttKRYCG",
+  "5V7WoYwRXtheRjhOjgUeR5",
+  "5BrjR0P59l9SsbODztqs3q",
+  "7bpWEp24oHgUs08ImjakfU",
+  "45gsxfnJ5Nt2RZp82SQenc",
+  "5VPw18vPykfaX5s3jiS0Ox",
+];
