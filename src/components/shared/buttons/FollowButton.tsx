211fd2c0aa13ed5184653ba02a70d207ec0deedf
Namig <namig.pashayev1@gmail.com>
2026-01-02 01:56:17 +0400

    Create FollowButton component

---
+++ src/components/shared/buttons/FollowButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { useFollowedArtists } from "@/lib/contexts/FollowedArtistsContext";
+
+interface Props {
+    artistId: string;
+}
+
+export default function FollowButton({ artistId }: Props) {
+    const { isFollowed, follow, unfollow } = useFollowedArtists();
+    const followed = isFollowed(artistId);
+
+    const handleClick = () => {
+        if (followed) {
+            unfollow(artistId);
+        } else {
+            follow(artistId);
+        }
+    }
+
+    return (
+        <button onClick={handleClick} className="control-btn rounded-3xl border-2 border-white px-4 py-1">
+            {followed ? "Following" : "Follow"}
+        </button>
+    );
+};
